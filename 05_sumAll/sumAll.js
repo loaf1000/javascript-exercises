3a46ab4c829005768d26f2b6d23f4098798c9a1d
Loaf <loaf1000@gmail.com>
2024-04-24 09:46:11 -0700

    sum All done

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
 
+    if (num1 < 0 || num2 < 0)
+    {
+        return "ERROR";
+    }
+    else if (!Number.isInteger(num1) || !Number.isInteger(num2))
+    {
+        return "ERROR";
+    }
+
+    const startingNumber = (num1 < num2) ? num1 : num2;
+    const endingNumber = (num1 > num2) ? num1 : num2;
+    let sum = 0;
+
+    for (i = startingNumber; i < endingNumber + 1; i++)
+    {
+        sum += i;
+    }
+    
+    return sum;
 };
 
 // Do not edit below this line
