c343d91ae32d41a1267f0413e4f7cb785545a754
Loaf <loaf1000@gmail.com>
2024-04-23 21:50:33 -0700

    repeat string done

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,4 +1,17 @@
-const repeatString = function() {
+const repeatString = function(string, timesToRepeat) {
+    let finalString = "";
+
+    if(Math.sign(timesToRepeat) < 0)
+    {
+        return 'ERROR';
+    }
+
+    for(index = 0; index < timesToRepeat; index++)
+    {
+        finalString += string;
+    }
+
+    return finalString;
 
 };
 
