95e88603a77564d33f783b5720189be8c3e8d2d6
Loaf <loaf1000@gmail.com>
2024-04-24 09:29:41 -0700

    remove from array finished

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...elementsToRemove) {
+    for (elementToRemove of elementsToRemove)
+    {
+        while(array.indexOf(elementToRemove) != -1)
+        {
+            let removalIndex = array.indexOf(elementToRemove);
+            array.splice(removalIndex, 1);
+        }
+    }
+    return array;
 
 };
 
